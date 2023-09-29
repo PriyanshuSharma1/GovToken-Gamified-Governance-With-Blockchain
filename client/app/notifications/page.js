7f036356cd0c5575a81123e52f570f58c9c486d8
Priyanshu <psneb420@gmail.com>
2023-09-30 00:01:22 +0545

    Struggling to vreate the sidebar updates box

--- client/app/notifications/page.js
+++ client/app/notifications/page.js
@@ -1,20 +1,25 @@
 export default function notificationsPage() {
   return (
     <div>
-      <h1> Feed</h1>
-      <div className="box">
-        <h1 style={{ color: "green" }}>Welcome To My world !!</h1>
-        <p>let's write here.</p>
-        <button className="appreciate-button">Appreciate</button>
+      <h1 className="content"> Feed</h1>
+      <div className="feed-content">
+        <div className="box">
+          <h1 style={{ color: "green" }}>Welcome To My world !!</h1>
+          <p>let's write here.</p>
+          <button className="appreciate-button">Appreciate</button>
+        </div>
+        <div className="box">
+          <h1 style={{ color: "green" }}>Welcome To My world !!</h1>
+          <p>let's write here.</p>
+          <button className="appreciate-button">Appreciate</button>
+        </div>
+        <div className="box">
+          <h1 style={{ color: "green" }}>Welcome To My world !!</h1>
+          <p>let's write here.</p>
+          <button className="appreciate-button">Appreciate</button>
+        </div>
       </div>
-      <div className="box">
-        <h1 style={{ color: "green" }}>Welcome To My world !!</h1>
-        <p>let's write here.</p>
-      </div>
-      <div className="box">
-        <h1 style={{ color: "green" }}>Welcome To My world !!</h1>
-        <p>let's write here.</p>
-      </div>
+      <div className="vertical-rectangular-block"></div>
     </div>
   );
 }
