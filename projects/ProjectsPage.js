3bda67aa32b2939570f650a8987e13214f76b46b
subal000 <subal.ghimire2396@gmail.com>
2023-09-29 23:48:01 +0545

    community projects

---
+++ projects/ProjectsPage.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from './ProjectList';
+
+function ProjectsPage() {
+  const [projects,] = useState(MOCK_PROJECTS);
+ 
+  return (
+    <>
+    <h1>Community Events</h1>
+    <ProjectList projects = {projects}/>
+    </>
+  )
+}
+export default ProjectsPage
